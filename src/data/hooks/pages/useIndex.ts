c97fe1bfa1e3ad518549fec694c8aebf22ca66ef
felipe <fl.louzeiro@gmail.com>
2022-06-04 16:12:47 -0300

    feat: implementa busca por pets na api

--- src/data/hooks/pages/useIndex.ts
+++ src/data/hooks/pages/useIndex.ts
@@ -1,33 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Pet } from '../../@types/patch'
+import { ApiService } from '../../services/ApiServices'
 
 export function useIndex() {
-  function toAdopt() {}
-
-  const [petList, setPetList] = useState([
-    {
-      id: 1,
-      name: 'bidu',
-      description:
-        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur temporibus cupiditate inventore repudiandae ullam ratione, reiciendis adipisci eius quibusdam, veritatis non? Vitae architecto soluta perferendis quasi eaque placeat, totam dolores.',
-      image:
-        'https://www.zooplus.pt/magazine/wp-content/uploads/2021/03/kitten-sitzt-boden-768x512-1.jpeg',
-    },
-    {
-      id: 1,
-      name: 'bidu',
-      description: 'Lorem ipsum dol totam dolores.',
-      image:
-        'https://www.zooplus.pt/magazine/wp-content/uploads/2021/03/kitten-sitzt-boden-768x512-1.jpeg',
-    },
-  ])
-
+  const [petList, setPetList] = useState<Pet[]>([])
   const [selectedPet, setSelectedPet] = useState<Pet | null>(null)
   const [email, setEmail] = useState<string>('')
   const [donation, setDonation] = useState<string>('')
   const [confirmationMessage, setConfirmationMessage] = useState<string>(
     'Adoção confirmada, verifique sua caixa de email'
   )
+
+  function toAdopt() {}
+
+  useEffect(() => {
+    ApiService.get('/pets').then((response) => {
+      setPetList(response.data)
+    })
+  }, [])
 
   return {
     petList,
