bb2234a368105be45b640de8f334cf9e1d1aebd8
felipe <fl.louzeiro@gmail.com>
2022-06-03 21:31:19 -0300

    feat: incorporando lista de pets na aplicação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import { List } from '../ui/components/List'
 import { Title } from '../ui/components/title'
 
 const Home: NextPage = () => {
@@ -13,8 +14,24 @@ const Home: NextPage = () => {
           </span>
         }
       />
+      <List
+        pets={[
+          {
+            id: 1,
+            name: 'bidu',
+            description:
+              'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur temporibus cupiditate inventore repudiandae ullam ratione, reiciendis adipisci eius quibusdam, veritatis non? Vitae architecto soluta perferendis quasi eaque placeat, totam dolores.',
+            image:
+              'https://www.zooplus.pt/magazine/wp-content/uploads/2021/03/kitten-sitzt-boden-768x512-1.jpeg',
+          },
+        ]}
+      />
     </>
   )
 }
 
 export default Home
+// id: number
+// name: string
+// description: string
+// image: string
